4c07c9a06d1bc8c5fdd00c515c6f31bae0b3281c
MaironL <Mairon_488@outlook.com>
2022-01-08 20:36:06 -0400

    Arquitectura del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import AppRouters from './infrastructure/routes/AppRouters';
+
+const App = () => {
+  return <AppRouters />;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyle from './globalStyle';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
