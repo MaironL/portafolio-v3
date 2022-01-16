c4c56e7a97762abfae8638bfe974d6da303902f8
MaironL <Mairon_488@outlook.com>
2022-01-15 22:48:11 -0400

    Arreglos a experiencia laboral

--- src/infrastructure/assets/data/jobs.ts
+++ src/infrastructure/assets/data/jobs.ts
@@ -10,9 +10,9 @@ export const jobs: jobsInterface[] = [
   {
     title: 'Desarrollador Front-End Jr.',
     duties: [
-      'Desarrollo de screens en multiples proyectos, junto con creacion de formas con sus validaciones mediante el uso de Formik',
-      'Actualizacion del layout de la pagina principal de la empresa para incorporacion de un layout responsivo con CSS Grid',
-      'Realizacion de peticiones a API Rest, y actualizacion de los datos en el store de Redux',
+      'Desarrollo de screens en multiples proyectos, junto con creacion de formas con sus validaciones mediante el uso de Formik.',
+      'Actualizacion del layout de la pagina principal de la empresa para incorporacion de un layout responsivo con CSS Grid.',
+      'Realizacion de peticiones a API Rest, y actualizacion de los datos en el store de Redux.',
     ],
     dates: 'Mayo 2021 - Enero 2022',
     company: 'Ragnardsoft',
