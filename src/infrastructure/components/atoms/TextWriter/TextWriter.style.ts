44dc9d58fd8b4d4c9e6849ab8bc4b4b165a11626
MaironL <Mairon_488@outlook.com>
2022-05-09 21:58:08 -0400

    textWriter 280px responsiveness support

--- src/infrastructure/components/atoms/TextWriter/TextWriter.style.ts
+++ src/infrastructure/components/atoms/TextWriter/TextWriter.style.ts
@@ -24,8 +24,12 @@ export const staticText = styled.div`
 export const dynamicTextList = styled.ul`
   margin-left: 5px;
   width: 100%;
-  height: 35px;
+  height: 60px;
   overflow: hidden;
+
+  @media screen and (min-width: 320px) {
+    height: 35px;
+  }
 
   @media screen and (min-width: 600px) {
     height: 1.9rem;
@@ -37,11 +41,32 @@ export const dynamicTextList = styled.ul`
 `;
 
 export const dynamicTextWord = styled.li`
-  line-height: 35px;
+  display: block;
+  height: 70px;
+  padding-top: 8px;
   font-size: 1rem;
   color: #a9a9a9;
   position: relative;
   top: 0;
+
+  animation: textSlide 30s steps(5) infinite;
+  @keyframes textSlide {
+    100% {
+      top: -350px;
+    }
+  }
+
+  @media screen and (min-width: 320px) {
+    height: auto;
+    padding-top: 0;
+    line-height: 35px;
+    animation: textSlide 30s steps(5) infinite;
+    @keyframes textSlide {
+      100% {
+        top: -175px;
+      }
+    }
+  }
 
   @media screen and (min-width: 600px) {
     font-size: 1.2rem;
@@ -49,13 +74,6 @@ export const dynamicTextWord = styled.li`
 
   @media screen and (min-width: 900px) {
     font-size: 1.3rem;
-  }
-
-  animation: textSlide 30s steps(5) infinite;
-  @keyframes textSlide {
-    100% {
-      top: -175px;
-    }
   }
 `;
 
@@ -67,12 +85,12 @@ export const dynamicText = styled.span`
     content: '';
     position: absolute;
     left: 0;
-    bottom: -5px;
-    height: 28px;
-    width: 100%;
+    top: 0px;
+    height: 40px;
+    width: 90vw;
     background-color: var(--clr-bg-2);
     border-left: 2px solid var(--clr-primary-5);
-    animation: textTyping 6s steps(40) infinite;
+    animation: textTyping 6s steps(31) infinite;
 
     @keyframes textTyping {
       0% {
@@ -81,18 +99,54 @@ export const dynamicText = styled.span`
       }
 
       50% {
-        left: 100%;
+        left: 65vw;
         margin: 0 -35px 0 35px;
       }
 
       75% {
-        left: 100%;
+        left: 65vw;
         margin: 0 -35px 0 35px;
         border-left: 2px solid var(--clr-primary-5);
       }
 
       100% {
         border-left: 20px solid var(--clr-logo-orange);
+      }
+    }
+  }
+
+  @media screen and (min-width: 320px) {
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: -5px;
+      height: 28px;
+      width: 100%;
+      background-color: var(--clr-bg-2);
+      border-left: 2px solid var(--clr-primary-5);
+      animation: textTyping 6s steps(40) infinite;
+
+      @keyframes textTyping {
+        0% {
+          left: 0%;
+          margin: 0px;
+        }
+
+        50% {
+          left: 100%;
+          margin: 0 -35px 0 35px;
+        }
+
+        75% {
+          left: 100%;
+          margin: 0 -35px 0 35px;
+          border-left: 2px solid var(--clr-primary-5);
+        }
+
+        100% {
+          border-left: 20px solid var(--clr-logo-orange);
+        }
       }
     }
   }
